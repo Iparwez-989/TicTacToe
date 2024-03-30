4314fbedd4fe696046967818990f9d68bbf17890
Irfan Parwez <136509687+Iparwez-989@users.noreply.github.com>
2024-03-30 23:48:58 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 function Button(props) {
   return (
     <div className='button'>
+
+{/*       Button */}
       <button onClick={props.click} className='btn'>{props.value}</button>
     </div>
   )
